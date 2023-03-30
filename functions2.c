8cfd9bd808eb9179c0988489e64699ad91953b23
Phxsha <phashateboho@gmail.com>
2023-03-30 09:29:43 -0700

    Simple Shell

---
+++ functions2.c
@@ -0,0 +1,107 @@
+#include "shell.h"
+
+/**
+* changing_tab - changes tabs for spaces
+* @string: input string passed
+* Return: 0
+*/
+void changing_tab(char *string)
+{
+	int len = _strlen(string);
+	int i = 0;
+
+	for (; i < len; i++)
+	{
+		if (string[i] == '\t')
+			string[i] = ' ';
+	}
+}
+
+/**
+* count_spaces - count spaces
+* @string: input string passed
+* Return: count
+*/
+int count_spaces(char *string)
+{
+	int len = _strlen(string);
+	int i = 0;
+	int count = 1;
+
+	for (; i < len; i++)
+	{
+		if (string[i] == ' ' && string[i + 1] != ' ' && string[i + 1] != '\n')
+			count += 1;
+	}
+	return (count);
+}
+
+/**
+* count_paths - count spaces
+* @string: input string passed
+* Return: count + 1
+*/
+int count_paths(char *string)
+{
+	int len = _strlen(string);
+	int i = 0;
+	int count = 0;
+
+	for (; i < len; i++)
+	{
+		if (string[i] == ':')
+			count += 1;
+	}
+	return (count + 1);
+}
+
+/**
+* clean_spaces - count spaces
+* @buffer: input string passed
+* Return: count + 1
+*/
+char *clean_spaces(char *buffer)
+{
+	int len, first_char_position, i = 0;
+	char *new_buffer, *true_buffer;
+
+	new_buffer = _strdup(buffer);
+	if (new_buffer[0] != ' ')
+		return (new_buffer);
+	while (new_buffer[i] == ' ')
+		i++;
+	first_char_position = i;
+	while (new_buffer[i])
+		i++;
+	len = i - first_char_position;
+	true_buffer = malloc(sizeof(char) * (len + 1));
+	i = first_char_position;
+	while (new_buffer[i])
+	{
+		true_buffer[i - first_char_position] = new_buffer[i];
+		i++;
+	}
+	true_buffer[i - first_char_position] = '\0';
+	free(new_buffer);
+	return (true_buffer);
+}
+
+/**
+* parent_wait - count spaces
+* @child_pid: child id number
+* @status: int status of the process
+* Return: 0 if it fails 1 if not
+*/
+int parent_wait(int child_pid, int *status)
+{
+	if (waitpid(child_pid, status, 0) == -1)
+	{
+		perror("Waitpid failed");
+		return (0);
+	}
+	if (WIFEXITED(*status))
+	{
+		return (WEXITSTATUS(*status));
+	}
+	return (1);
+}
---
+++ shell.c
@@ -0,0 +1,141 @@
+#include "shell.h"
+/**
+* main - main function of shell program.
+* @argc: int saving arguments
+* @argv: char storing arguments
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	size_t buffer = 0;
+	char *string = NULL;
+	ssize_t line = 0;
+	int status = 0;
+	char *cleaned_string;
+	(void) argc;
+
+	signal(SIGINT, stop_ctrl);
+
+	while (1)
+	{
+	if (isatty(STDIN_FILENO) == 1)
+	write(1, ":$ ", 11);
+	line = getline(&string, &buffer, stdin);
+	if (line == -1)
+	{
+	if (isatty(STDIN_FILENO) == 1)
+	write(1, "\n", 1);
+	free(string);
+	break;
+	}
+	changing_tab(string);
+	if (*string == '\0')
+	continue;
+	if (not_empty(string) == -1)
+	{
+	cleaned_string = clean_spaces(string);
+	status = reading(strtok(cleaned_string, "\n"), argv[0], string);
+	}
+	}
+	return (status);
+}
+
+/**
+* reading - parser whatever you just wrote on the shell
+* @s: statement to be read
+* @shellname: name of the shell (previously declared on struct)
+* @string: string to be passed
+* Return: 0
+*/
+int reading(char *s, char *shellname, char *string)
+{
+	char *token = NULL;
+	char **command_arr;
+	int i = 0;
+	int ret;
+	int count = count_spaces(s);
+
+	command_arr = malloc(sizeof(char *) * (count + 1));
+	token = strtok(s, " ");
+	if (_strcmp(s, "exit") == 0)
+	{
+	free(s);
+	free(string);
+	free(command_arr);
+	exit(0);
+	}
+	if (_strcmp(s, "env") == 0)
+	{
+	free(s);
+	free(command_arr);
+	return (_printenv());
+	}
+	while (token)
+	{
+	command_arr[i] = _strdup(token);
+	i++;
+	token = strtok(NULL, " ");
+	}
+	if (command_arr == NULL)
+	{
+	free(s);
+	}
+	command_arr[i] = NULL;
+	free(s);
+	ret = execute(command_arr, shellname);
+	i = 0;
+	while (i < count)
+	free(command_arr[i++]);
+	free(command_arr);
+	return (ret);
+}
+
+/**
+* execute - Runs function
+* @cmd: command to be ran.
+* @shellname: name of the shell (previously declared on struct)
+* Return: 0
+*/
+int execute(char **cmd, char *shellname)
+{
+	char *path = NULL;
+	pid_t child;
+	char *command = NULL;
+	int status, stat;
+	const char *dir = cmd[1];
+
+	command = cmd[0];
+	if (_strcmp(command, "cd") == 0)
+	{
+	if (dir == NULL)
+	return (chdir(_getenv("HOME")));
+	if (chdir(dir) != 0)
+	{
+	perror(dir);
+	return (1);
+	}
+	else
+	return (chdir(dir));
+	}
+	path = pathfinder(command);
+
+	if (path == NULL && _strcmp(command, "cd") != 0)
+	{
+	write(2, shellname, _strlen(shellname));
+	write(2, " :1 ", 5);
+	write(2, command, _strlen(command));
+	write(2, ": not found\n", 12);
+	return (127);
+	}
+	child = fork();
+	if (child != 0)
+	stat = parent_wait(child, &status);
+	else if (child == 0 && _strcmp(command, "cd") != 0)
+	{
+	execve(path, cmd, environ);
+	perror("Error:");
+	exit(-1);
+	}
+	free(path);
+	return (stat);
+}
